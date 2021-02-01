7bd30e2bc191255543fb8e740f5b7ce3446733b9
harry obaseki <harryobas@gmail.com>
2021-02-01 05:49:48 +0100

    first commit

---
+++ lib/robot/factory.rb
@@ -0,0 +1,8 @@
+require "robot/factory/version"
+
+module Robot
+  module Factory
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/robot/factory/version.rb
@@ -0,0 +1,5 @@
+module Robot
+  module Factory
+    VERSION = "0.1.0"
+  end
+end
